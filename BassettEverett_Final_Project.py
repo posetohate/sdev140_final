af8b69c738230e3c6f97aa920083294b7549b7ae
posetohate <104879489+posetohate@users.noreply.github.com>
2022-05-03 23:51:02 -0400

    Add files via upload

---
+++ BassettEverett_Final_Project.py
@@ -0,0 +1,19 @@
+"""
+Program:BassettEverett_Final_Project.py
+Author:Everett Bassett
+Last date modified: 5/3/2022
+
+The purpose of this program is to help the user find vending machines
+in their area, as well as providing inventories of said machines, and
+pricing of the products inside of the machine.
+
+"""
+from tkinter import *
+
+root = Tk()
+# Creating a Label Widget
+myLabel = Label(root, text="SodaGo - Vending Machine Finder!")
+
+myLabel.pack()
+
+root.mainloop()
